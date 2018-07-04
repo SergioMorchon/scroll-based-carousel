97d21ee4e22e97d89ce5149018af392ca3c4f860
Sergio Morchón Poveda <smorchon@tuenti.com>
2018-07-04 15:56:50 +0200

    Improve demo

--- www/index.js
+++ www/index.js
@@ -1,19 +1,24 @@
 import Scrollousel from './scrollousel.js';
 
-/** Credits to https://math.stackexchange.com/a/121755 */
+/* Function credits to https://math.stackexchange.com/a/121755 */
 const createFancyEasyInOut = factor => x => x ** factor / (x ** factor + (1 - x) ** factor);
+
+const updateSlideIndex = index => {
+    document.getElementById('slide-index').value = index;
+};
 
 const carousel = new Scrollousel(document.querySelector('.carousel'), {
     scrollableSelector: '.carousel',
     sliderSelector: '.slider',
-    onIndexChange: index => {
-        document.getElementById('output').innerText = `Slide #${index}`;
-    },
+    onIndexChange: updateSlideIndex,
     moveAnimation: {
         duration: 777,
         timingFunction: createFancyEasyInOut(3),
     },
 });
+
 document.getElementById('slide-index').addEventListener('change', e => {
     carousel.index = Number(e.srcElement.value);
 });
+
+updateSlideIndex(carousel.index);
